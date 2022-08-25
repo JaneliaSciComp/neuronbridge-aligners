8a8a987d101502bb56020da2b84960efe748d8a5
Cristian Goina <goinac@hhmi.org>
2022-08-25 10:51:03 -0400

    cleanup on the script and started the docker file

--- vnc-aligner/aligner/20x40xVNC_Align_CMTK.sh
+++ vnc-aligner/aligner/20x40xVNC_Align_CMTK.sh
@@ -2,180 +2,79 @@
 #
 # 20x 40x VNC aligner by Hideo Otsuna
 #
-testmode=0
 
-if [[ $testmode != 1 ]]; then
-  DIR=$(cd "$(dirname "$0")"; pwd)
-  . $DIR/common.sh
-  SKIP_PREALIGNER="false"
-  parseParameters "$@"
-  nc82decision="Signal_amount"
+InputFilePath=$1
+NSLOTS=$2
+OUTPUTORI=$3
+nc82decision="Signal_amount"
 
+OUTPUTNAME=$(basename $InputFilePath)
+# final output name is the filename without extension
+OUTPUTNAME=${OUTPUTNAME%.*} # final output name is the filename without extension
 
-  # Tools
-  CMTK=/opt/CMTK/bin
-  FIJI=/opt/Fiji/ImageJ-linux64
-  Vaa3D=/opt/Vaa3D/vaa3d
-  MACRO_DIR=/opt/aligner/fiji_macros
+WORK_DIRPRE=$(dirname $InputFilePath)
+WORK_DIR="${WORK_DIRPRE}/${OUTPUTNAME}"
 
-  # Fiji macros
-  NRRDCONV=$MACRO_DIR"/nrrd2v3draw.ijm"
-  PREPROCIMG=${MACRO_DIR}"VNC_preImageProcessing_Pipeline_07_29_2022.ijm"
-  TWELVEBITCONV=$MACRO_DIR"/12bit_Conversion.ijm"
-NRRDCOMP=$MACRO_DIR"/nrrd_compression.ijm"
-
-  Path=$INPUT1_FILE
-  objective=$INPUT1_OBJECTIVE
-  OUTPUT=$WORK_DIR"/Output"
-  FINALOUTPUT=$WORK_DIR"/FinalOutputs"
-  TempDir=${MACRO_DIR}"Template"
-  MIPTempDir=${MACRO_DIR}"Template_MIP"
-
-  DEBUG_DIR=$FINALOUTPUT"/debug"
-  mkdir -p $DEBUG_DIR
-  echo "DEBUG_DIR: $DEBUG_DIR"
+if [[ ! -d ${OUTPUTORI} ]]; then
+    mkdir -p ${OUTPUTORI}
 fi
 
-echo "$testmode; "$testmode
-# For TEST ############################################
-if [[ $testmode == "1" ]]; then
-    echo "Test mode"
+OUTPUT="${OUTPUTORI}/${OUTPUTNAME}"
 
-    InputFilePath=$1
-    NSLOTS=$2
-    OUTPUTORI=$3
-    nc82decision="Signal_amount"
+# Tools
+CMTK=/opt/CMTK/bin
+FIJI=/opt/Fiji/ImageJ-linux64
+Vaa3D=/opt/Vaa3D/vaa3d
+MACRO_DIR=/opt/aligner/fiji_macros
 
-    end=${InputFilePath##*/}
-    num1=$((${#InputFilePath} - ${#end}))
+GENDER_TEMPLATE_SELECTOR="u"
+objective="40x"
 
-    OUTPUTNAME=${InputFilePath#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-    OUTPUTNAME=${OUTPUTNAME#*/}
-
-    echo "OUTPUTNAME; "${OUTPUTNAME%.*}
-
-    WORK_DIRPRE=${InputFilePath:0:num1-1}
-    WORK_DIR=${WORK_DIRPRE}"/"${OUTPUTNAME%.*}
-
-    if [[ ! -d ${OUTPUTORI} ]]; then
-        mkdir ${OUTPUTORI}
-    fi
-
-    OUTPUT=${OUTPUTORI}"/"${OUTPUTNAME%.*}
-
-    ####### tools location setting #######################################################
-    MACRO_DIR="/Users/otsunah/test/VNC_aligner_local_ver/"
-    FIJI=/Applications/Fiji_copy.app/Contents/MacOS/ImageJ-macosx
-    CMTK=/Applications/Fiji_copy.app/bin/cmtk
-
-    TEMPNUMBER=1
-    echo "TEMPNUMBER; "$TEMPNUMBER
-
-    INPUT1_GENDER="u"
-    objective="40x"
-
-    TempDir=${MACRO_DIR}"Template"
-    MIPTempDir=${MACRO_DIR}"Template_MIP"
-    NRRDCONV=${MACRO_DIR}"nrrd2v3draw_MCFO.ijm"
-    PREPROCIMG=${MACRO_DIR}"VNC_preImageProcessing_Pipeline_07_29_2022.ijm"
-    NRRDCOMP=$MACRO_DIR"/nrrd_compression.ijm"
+NRRDCONV="${MACRO_DIR}/nrrd2v3draw_MCFO.ijm"
+PREPROCIMG="${MACRO_DIR}/VNC_preImageProcessing_Pipeline_07_29_2022.ijm"
+NRRDCOMP="$MACRO_DIR/nrrd_compression.ijm"
  
-fi
-
 export CMTK_WRITE_UNCOMPRESSED=1
 
 # "-------------------Template----------------------"
-JRC2018_VNC_Unisex1micron=$TempDir"/JRC2018_VNC_UNISEX_1micron.nrrd"
+TEMPLATE_DIR=${TEMPLATE_DIR:-"/data/alignment_templates"}
+TemplatesDir=`realpath ${TEMPLATE_DIR}`
+MIPTemplatesDir="${MACRO_DIR}/Template_MIP"
 
-if [[ ${TEMPNUMBER} == 1 ]]; then
+JRC2018_VNC_Unisex1micron="$TemplatesDir/JRC2018_VNC_UNISEX_1micron.nrrd"
+
+TEMPLATE_SELECTOR=1
+if [[ ${TEMPLATE_SELECTOR} == 1 ]]; then
     TEMPNAME="JRC2018_VNC_UNISEX_447_G15.nrrd"
-elif [[ $TEMPNUMBER == 2 ]]; then
+elif [[ $TEMPLATE_SELECTOR == 2 ]]; then
     TEMPNAME="JRC2018_VNC_UNISEX_63x_lowG.nrrd"
-elif [[ $TEMPNUMBER == 3 ]]; then
+elif [[ $TEMPLATE_SELECTOR == 3 ]]; then
     TEMPNAME="RC2018_VNC_FEMALE_447_G15.nrrd"
-elif [[ $TEMPNUMBER == 4 ]]; then
+elif [[ $TEMPLATE_SELECTOR == 4 ]]; then
     TEMPNAME="JRC2018_VNC_MALE_447_G15.nrrd"
 fi
 
-TARGET_TEMPLATE=$TempDir"/${TEMPNAME}"
+TARGET_TEMPLATE=$TemplatesDir"/${TEMPNAME}"
 
 echo "TEMPNAME; "$TEMPNAME
 
-
 # For Score ########################################
-if [[ $INPUT1_GENDER =~ "m" ]]; then
+if [[ $GENDER_TEMPLATE_SELECTOR =~ "m" ]]; then
     # male fly vnc
-    Tfile=${TempDir}"/MaleVNC2017.nrrd"
-    POSTSCOREMASK=$MACRO_DIR"/For_Score/Mask_Male_VNC.nrrd"
-elif [[ $INPUT1_GENDER =~ "f" ]]; then
+    Tfile=${TemplatesDir}"/MaleVNC2017.nrrd"
+    POSTSCOREMASK="${MACRO_DIR}/For_Score/Mask_Male_VNC.nrrd"
+elif [[ $GENDER_TEMPLATE_SELECTOR =~ "f" ]]; then
     # female fly vnc
-    Tfile=${TempDir}"/FemaleVNCSymmetric2017.nrrd"
-    POSTSCOREMASK=$MACRO_DIR"/For_Score/flyVNCtemplate20xA_CLAHE_MASK2nd.nrrd"
+    Tfile="${TemplatesDir}/FemaleVNCSymmetric2017.nrrd"
+    POSTSCOREMASK="${MACRO_DIR}/For_Score/flyVNCtemplate20xA_CLAHE_MASK2nd.nrrd"
 else
     # unisex fly vnc
-    Tfile=${TempDir}"/FemaleVNCSymmetric2017.nrrd"
-    POSTSCOREMASK=$MACRO_DIR"/For_Score/JRC2018_VNC_UNISEX_447_3DMASK.nrrd"
+    Tfile=${TemplatesDir}"/FemaleVNCSymmetric2017.nrrd"
+    POSTSCOREMASK="${MACRO_DIR}/For_Score/JRC2018_VNC_UNISEX_447_3DMASK.nrrd"
 fi
 
-POSTSCORE=${MACRO_DIR}"/Score_Generator_Cluster.ijm"
-MIPGENERATION=${MACRO_DIR}"/Color_Depth_MIP_batch_0404_2019_For_Pipeline_packbits.ijm"
-
-#
-# Expand resolutions from TMOG
-#
-function expandRes() {
-    local _res="$1"
-    local _result_var="$2"
-    if [[ $_res == "0.44" ]]; then
-        result="0.4413373"
-    elif [[ $_res == "0.52" ]]; then
-        result="0.5189161"
-    elif [[ $_res == "0.62" ]]; then
-        result="0.621481"
-    else
-        result=$_res
-    fi
-    eval $_result_var="'$result'"
-}
-
-# Convert multiple NRRD files into a single v3draw file.
-# Params for this function are the same as for the Fiji macro, a single parameter with comma-delimited file names:
-#   "output.v3draw,input1.nrrd,input2.nrrd..."
-function nrrd2Raw() {
-    local _PARAMS="$1"
-    OUTPUTRAW=${_PARAMS%%,*}
-    if [[ -e $OUTPUTRAW ]]; then
-        echo "Already exists: $OUTPUTRAW"
-    else
-        TS=`date +%Y%m%d-%H%M%S`
-        LOGFILE="$DEBUG_DIR/raw-${TS}.log"
-        echo "+----------------------------------------------------------------------+"
-        echo "| Running NRRD -> v3draw conversion"
-        echo "| $FIJI --headless -macro $NRRDCONV $_PARAMS >$LOGFILE"
-        echo "+----------------------------------------------------------------------+"
-        START=`date '+%F %T'`
-        $FIJI --headless -macro $NRRDCONV $_PARAMS >$LOGFILE 2>&1
-        STOP=`date '+%F %T'`
-        if [[ ! -e $OUTPUTRAW ]]; then
-            echo -e "Error: NRRD -> raw conversion failed"
-            exit -1
-        fi
-        echo "nrrd_raw_conversion start: $START"
-        echo "nrrd_raw_conversion stop: $STOP"
-    fi
-}
+POSTSCORE="${MACRO_DIR}/Score_Generator_Cluster.ijm"
+MIPGENERATION="${MACRO_DIR}/Color_Depth_MIP_batch_0404_2019_For_Pipeline_packbits.ijm"
 
 #
 # Reverse a stack in some dimension
@@ -188,7 +87,6 @@ function flip() {
     if [[ -e $_output ]]; then
         echo "Already exists: $_output"
     else
-        #---exe---#
         message " Flipping $_input with $_op"
         $Vaa3D -x ireg -f $_op -i $_input -o $_output
         echo ""
@@ -206,22 +104,22 @@ function scoreGen() {
 	scorepath="$OUTPUT/${tempname}_Score.property"
 	
 	if [[ -e ${scorepath} ]]; then
-	echo "Already exists: $scorepath"
+	    echo "Already exists: $scorepath"
 	else
-	echo "+---------------------------------------------------------------------------------------+"
-	echo "| Running Score generation"
-	echo "| $FIJI --headless -macro $POSTSCORE $OUTPUT/,$_outname,$NSLOTS,$_scoretemp"
-	echo "+---------------------------------------------------------------------------------------+"
+	    echo "+---------------------------------------------------------------------------------------+"
+	    echo "| Running Score generation"
+	    echo "| $FIJI --headless -macro $POSTSCORE $OUTPUT/,$_outname,$NSLOTS,$_scoretemp"
+        echo "+---------------------------------------------------------------------------------------+"
+    
+        START=`date '+%F %T'`
+        # Expect to take far less than 1 hour
+	    # Alignment Score generation:ZNCC, does not need Xvfb
 	
-	START=`date '+%F %T'`
-	# Expect to take far less than 1 hour
-	# Alignment Score generation:ZNCC, does not need Xvfb
-	
-	$FIJI --headless -macro ${POSTSCORE} $OUTPUT/,$_outname,$NSLOTS,$_scoretemp
-	STOP=`date '+%F %T'`
-	
-	echo "ZNCC JRC2018 score generation start: $START"
-	echo "ZNCC JRC2018 score generation stop: $STOP"
+	    $FIJI --headless -macro ${POSTSCORE} $OUTPUT/,$_outname,$NSLOTS,$_scoretemp
+	    STOP=`date '+%F %T'`
+
+	    echo "ZNCC JRC2018 score generation start: $START"
+	    echo "ZNCC JRC2018 score generation stop: $STOP"
 	fi
 }
 
@@ -283,7 +181,6 @@ function reformatAll() {
         fi
 	done
 }
-
 
 # write output properties for JACS
 function writeProperties() {
@@ -371,32 +268,31 @@ if [[ ! -e $FIJI ]]; then
     exit 1
 fi
 
-
-if [[ ${INPUT1_GENDER} == "f" ]]; then
+if [[ ${GENDER_TEMPLATE_SELECTOR} == "f" ]]; then
     genderT="FEMALE"
-    oldVNC=$TempDir"/FemaleVNCSymmetric2017.nrrd"
-    reformat_JRC2018_to_oldVNC=$TempDir"/Deformation_Fields/oldFemale_JRC2018_VNC_FEMALE"
+    oldVNC=$TemplatesDir"/FemaleVNCSymmetric2017.nrrd"
+    reformat_JRC2018_to_oldVNC=$TemplatesDir"/Deformation_Fields/oldFemale_JRC2018_VNC_FEMALE"
     OLDSPACE="FemaleVNCSymmetric2017_20x"
     iniT=${JRC2018_VNC_Female}
     OLDVOXELS="0.4612588x0.4612588x0.7"
-elif [[ $INPUT1_GENDER == "m" ]]; then
+elif [[ $GENDER_TEMPLATE_SELECTOR == "m" ]]; then
     genderT="MALE"
-    oldVNC=$TempDir"/MaleVNC2017.nrrd"
-    reformat_JRC2018_to_oldVNC=$TempDir"/Deformation_Fields/oldMale_JRC2018_VNC_MALE"
+    oldVNC=$TemplatesDir"/MaleVNC2017.nrrd"
+    reformat_JRC2018_to_oldVNC=$TemplatesDir"/Deformation_Fields/oldMale_JRC2018_VNC_MALE"
     OLDSPACE="MaleVNC2016_20x"
     iniT=$JRC2018_VNC_Male
     OLDVOXELS="0.4611222x0.4611222x0.7"
-elif [[ $INPUT1_GENDER == "u" ]]; then
+elif [[ $GENDER_TEMPLATE_SELECTOR == "u" ]]; then
     genderT="UNISEX"
     iniT=${JRC2018_VNC_Unisex1micron}
 else
-    echo "ERROR: invalid gender: $INPUT1_GENDER"
+    echo "ERROR: invalid gender: $GENDER_TEMPLATE_SELECTOR"
     exit 1
 fi
 
 filename="PRE_PROCESSED_"${genderT}
 
-echo "INPUT1_GENDER; "$INPUT1_GENDER
+echo "GENDER_TEMPLATE_SELECTOR; "$GENDER_TEMPLATE_SELECTOR
 echo "genderT; "$genderT
 echo "oldVNC; "$oldVNC
 echo "OLDSPACE; "$OLDSPACE
@@ -410,7 +306,7 @@ registered_initial_xform=$OUTPUT"/initial.xform"
 registered_affine_xform=$OUTPUT"/affine.xform"
 registered_warp_xform=$OUTPUT"/warp.xform"
 
-reformat_JRC2018_to_Uni=$TempDir"/Deformation_Fields/JRC2018_VNC_Unisex_JRC2018_"$genderT
+reformat_JRC2018_to_Uni="$TemplatesDir/Deformation_Fields/JRC2018_VNC_Unisex_JRC2018_$genderT"
 
 LOGFILE="${OUTPUT}/VNC_pre_aligner_log.txt"
 if [[ -e $LOGFILE ]]; then
@@ -423,17 +319,11 @@ else
     fi
     echo "+---------------------------------------------------------------------------------------+"
     echo "| Running Otsuna preprocessing step                                                     |"
-    echo "| $FIJI -macro $PREPROCIMG \"$OUTPUT/,$filename,$TempDir,$InputFilePath,ssr,$INPUT1_GENDER,NULL,$NSLOTS,$SHAPE_ANALYSIS\" >$DEBUG_DIR/preproc.log 2>&1 |"
+    echo "| $FIJI -macro $PREPROCIMG \"$OUTPUT/,$filename,$TemplatesDir,$InputFilePath,ssr,$GENDER_TEMPLATE_SELECTOR,NULL,$NSLOTS,$SHAPE_ANALYSIS\" >$DEBUG_DIR/preproc.log 2>&1 |"
     echo "+---------------------------------------------------------------------------------------+"
     START=`date '+%F %T'`
     # Expect to take far less than 1 hour
-
-    if [[ $testmode != 1 ]]; then
-        $FIJI -macro $PREPROCIMG "$OUTPUT/,$filename,$TempDir/,$InputFilePath,ssr,$INPUT1_GENDER,NULL,$NSLOTS,${SHAPE_ANALYSIS},${nc82decision}" >$DEBUG_DIR/preproc.log 2>&1
-    else
-        $FIJI --headless -macro $PREPROCIMG "$OUTPUT/,${filename},$TempDir/,$InputFilePath,ssr,$INPUT1_GENDER,NULL,$NSLOTS,${SHAPE_ANALYSIS},${nc82decision}" 
-    fi
-
+    $FIJI --headless -macro $PREPROCIMG "$OUTPUT/,${filename},$TemplatesDir/,$InputFilePath,ssr,$GENDER_TEMPLATE_SELECTOR,NULL,$NSLOTS,${SHAPE_ANALYSIS},${nc82decision}" >$DEBUG_DIR/preproc.log 2>&1
     STOP=`date '+%F %T'`
     echo "Otsuna preprocessing start: $START"
     echo "Otsuna preprocessing stop: $STOP"
@@ -509,10 +399,10 @@ fi
 
 FLIP_NEURON=""
 DEFFIELD="$registered_warp_xform"
-fn=${OUTPUTNAME%.*}
-sig=$OUTPUT"/"$fn
-TEMP="$TARGET_TEMPLATE"
-gsig=$OUTPUT"/"$filename
+fn=${OUTPUTNAME}
+sig="$OUTPUT/$fn"
+TEMP=$TARGET_TEMPLATE
+gsig="$OUTPUT/$filename"
 
 echo "reformat output; "${sig}"_01.nrrd"
 
@@ -525,28 +415,16 @@ if [[ ! -e ${sig}"_01.nrrd" ]]; then
     reformatAll "$gsig" "$TARGET_TEMPLATE" "$DEFFIELD" "$sig" "RAWOUT"
 fi
 
-echo "score log; "${OUTPUT}"/Score_log_"${TARGET_TEMPLATE%.*}".txt"
+echo "score log; ${OUTPUT}/Score_log_${TARGET_TEMPLATE%.*}.txt"
 
-if [[ ! -e ${OUTPUT}"/Score_log_"${TEMPNAME%.*}".txt" ]]; then
-    scoreGen $sig"_01.nrrd" ${TARGET_TEMPLATE} "score2018"
+if [[ ! -e "${OUTPUT}/Score_log_${TEMPNAME%.*}.txt" ]]; then
+    scoreGen "$sig_01.nrrd" ${TARGET_TEMPLATE} "score2018"
 fi
 
 for ((ii=1; ii<=4; ii++)); do
     if [[ -e "${sig}_0${ii}.nrrd" ]]; then
-        $FIJI --headless -macro ${MIPGENERATION} "${OUTPUT}/,${sig}_0"$ii".nrrd,${OUTPUTORI}/MIP/,${MIPTempDir}/,1.1,false,${NSLOTS}"
+        $FIJI --headless -macro ${MIPGENERATION} "${OUTPUT}/,${sig}_0"$ii".nrrd,${OUTPUTORI}/MIP/,${MIPTemplatesDir}/,1.1,false,${NSLOTS}"
     fi
 done
 
-if [[ $testmode != 1 ]]; then
-  writeProperties "$RAWOUT" "$FLIP_NEURON" "JRC2018_VNC_Unisex_40x_DS" "40x_DS" "0.461122x0.461122x0.70" "573x1119x219" "" "" "$main_aligned_file"
-fi
-
-#if [[ -e $sig"_01.nrrd" ]]; then
-  #  rm $OUTPUT"/"$filename"_01.nrrd"
-  #  rm $OUTPUT"/"$filename"_02.nrrd"
-#fi
-
-
 echo "$0 done"
-
-
